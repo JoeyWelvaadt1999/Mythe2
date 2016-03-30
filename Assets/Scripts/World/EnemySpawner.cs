f5b1ef47f55908064f49fa66f3532520a5814e06
BartGeest <kkcbart@live.nl>
2016-03-30 16:42:41 +0200

    30-3-2016 16:42 added EnemySpawning proto

---
+++ Assets/Scripts/World/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _enemyList = new List<GameObject>();
+
+    [SerializeField]
+    private float _overlapRadius;
+
+    private Collider2D _playerCollider;
+
+    private bool _enemySpawned = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SpawnEnemy();
+	}
+
+    void SpawnEnemy()
+    {
+        _playerCollider = Physics2D.OverlapCircle(this.transform.position, _overlapRadius);
+
+        if (_playerCollider)
+        {
+            GameObject randomEnemy = _enemyList[Random.Range(0, _enemyList.Count)];
+
+            if (!_enemySpawned)
+            {
+                Instantiate(randomEnemy, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+                _enemySpawned = true;
+            }
+        }
+    }
+}
